e62485893fb915d3883586553f7bb049f2e8b65a
Daniyal Abbasi <abbasi.daniyal98@gmail.com>
2025-03-05 01:30:04 -0500

    small fixes 2

--- linkedin_api/linkedin.py
+++ linkedin_api/linkedin.py
@@ -142,7 +142,11 @@ class Linkedin(object):
         if data and "status" in data and data["status"] != 200:
             self.logger.info("request failed: {}".format(data["message"]))
             return [{}]
-        while data and data["metadata"]["paginationToken"] != "":
+        while (
+            data
+            and "paginationToken" in data["metadata"]
+            and data["metadata"]["paginationToken"] != ""
+        ):
             if len(data["elements"]) >= post_count:
                 break
             pagination_token = data["metadata"]["paginationToken"]
@@ -178,7 +182,11 @@ class Linkedin(object):
         if data and "status" in data and data["status"] != 200:
             self.logger.info("request failed: {}".format(data["status"]))
             return [{}]
-        while data and data["metadata"]["paginationToken"] != "":
+        while (
+            data
+            and "paginationToken" in data["metadata"]
+            and data["metadata"]["paginationToken"] != ""
+        ):
             if len(data["elements"]) >= comment_count:
                 break
             pagination_token = data["metadata"]["paginationToken"]
@@ -982,14 +990,22 @@ class Linkedin(object):
                 paged_list_component_id
                 and "fsd_profilePositionGroup" in paged_list_component_id
             ):
-                pattern = r"urn:li:fsd_profilePositionGroup:\([A-z0-9]+,[A-z0-9]+\)"
+                pattern = r"urn:li:fsd_profilePositionGroup:\([^)]+\)"
                 match = re.search(pattern, paged_list_component_id)
                 return match.group(0) if match else None
 
         data = res.json()
 
         items = []
-        for item in data["included"][0]["components"]["elements"]:
+
+        max_items_index = max(
+            range(len(data["included"])),
+            key=lambda i: len(
+                data["included"][i].get("components", {}).get("elements", [])
+            ),
+        )
+
+        for item in data["included"][max_items_index]["components"]["elements"]:
             grouped_item_id = get_grouped_item_id(item)
             # if the item is part of a group (e.g. a company with multiple positions),
             # find the group items and parse them.
@@ -1214,7 +1230,7 @@ class Linkedin(object):
             f"/feed/dash/followingStates/{following_state_urn}", data=payload
         )
 
-        return res.status_code != 200
+        return res.status_code != 201
 
     def get_conversation_details(self, profile_urn_id):
         """Fetch conversation (message thread) details for a given LinkedIn profile.
@@ -1729,7 +1745,7 @@ class Linkedin(object):
             "count": 10,
             "q": "reactionType",
             "start": len(results),
-            "threadUrn": urn_id,
+            "threadUrn": f"urn:li:activity:{urn_id}",
         }
 
         res = self._fetch("/voyagerSocialDashReactions", params=params)
